b35bfcc5d2af73b8272691b51798bbfc9927b8fb
Bade-Gusi <2994938720@qq.com>
2026-05-29 21:22:48 +0800

    fix: GitHub Pages 静态导出 + 国内可访问

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/bsp-web',
   images: { unoptimized: true },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
